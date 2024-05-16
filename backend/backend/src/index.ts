e88fb59626f9d7c404eee024d3ad2a8b34ffbf26
Kiran <kiran.ram@gmail.com>
2024-05-16 21:47:37 +0100

    added schema signup route jwt implemented

--- backend/backend/src/index.ts
+++ backend/backend/src/index.ts
@@ -1,6 +1,16 @@
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 import { Hono } from 'hono'
+import { hashPassword } from './util/hashPassword';
+import { sign } from 'hono/jwt';
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string,
+    SECRET: string,
+  }
+}>();
+
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
@@ -8,6 +18,37 @@ app.get('/', (c) => {
 
 app.post('/api/v1/user/signup', async (c) => {
 
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const userPassword = body.password;
+
+  if (!userPassword) {
+    return c.json({error: "Incorrect Inputs"}, 400);
+  }
+
+  const hash = await hashPassword(userPassword);
+
+  const tag = body.tagline || "A hero was born";
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        password: hash,
+        name: body.name,
+      }
+    });
+
+    const jwt = await sign({id: user.id}, c.env.SECRET);
+    return c.json({jwt});
+
+  } catch (err) {
+    return c.json({error:"Incomplete Registration"}, 400);
+  }
   return c.text('signup route');
 })
 
